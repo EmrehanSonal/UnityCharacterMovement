a76c78bcfbc1ce08cfbc128f89fb8da36660d316
ErmeRG <emrehansonal@hotmail.com>
2026-05-19 18:35:21 +0300

    I fixed the camera. The character is turning with camera together.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private Transform cameraTransform;
     
    private void Update()
     {
         Vector2 inputVector = playerMovement.GetMovementVectorNormalized();
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
 
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDir = forward * inputVector.y + right * inputVector.x;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
        
     }
 }
