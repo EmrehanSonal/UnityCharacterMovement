e062bc5eb2a0f8417c14340cb784584448a47a57
ErmeRG <emrehansonal@hotmail.com>
2026-05-22 19:56:07 +0300

    I finish to make a crouch system.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float sprintSpeed = 9f;
 
+    [SerializeField] private CapsuleCollider capsuleCollider;
+    [SerializeField] private float standingHeight = 2f;
+    [SerializeField] private float crouchHeight = 1f;
+    [SerializeField] private float crouchSpeed = 3f;
+    private bool isCrouching;
 
     private void Awake()
     {
@@ -19,13 +24,41 @@ public class PlayerMovement : MonoBehaviour
             groundCheck = GetComponentInChildren<GroundCheck>();
         }
 
+        // try to auto-assign the capsule collider if not set in inspector
+        if (capsuleCollider == null)
+        {
+            capsuleCollider = GetComponent<CapsuleCollider>() ?? GetComponentInChildren<CapsuleCollider>();
+        }
+
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
-        //Sprinting
-        playerInputActions.Player.Sprint.performed += ctx => isSprinting = true;
-        playerInputActions.Player.Sprint.canceled += ctx => isSprinting = false;
-        
+        // Sprinting - use named handlers so we can unsubscribe cleanly
+        playerInputActions.Player.Sprint.performed += OnSprintPerformed;
+        playerInputActions.Player.Sprint.canceled += OnSprintCanceled;
+
+        playerInputActions.Player.Crouch.performed += OnCrouchPerformed;
+        playerInputActions.Player.Crouch.canceled += OnCrouchCanceled;
+    }
+
+    private void OnSprintPerformed(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        isSprinting = true;
+    }
+
+    private void OnSprintCanceled(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        isSprinting = false;
+    }
+
+    private void OnCrouchPerformed(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        StartCrouch();
+    }
+
+    private void OnCrouchCanceled(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        StopCrouch();
     }
 
     public Vector2 GetMovementVectorNormalized()
@@ -45,9 +78,52 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public float GetCurrentSpeed()
-    {   //if isSprinting is true  returns sprintSpeed, if false return walkSpeed. 
+    {  
+        if (isCrouching)
+        {
+            return crouchSpeed;
+        }
+         //if isSprinting is true  returns sprintSpeed, if false return walkSpeed. 
         return isSprinting ? sprintSpeed : walkSpeed;
     }
 
+    public void StartCrouch()
+    {
+        if (capsuleCollider == null)
+        {
+            return;
+        }
+
+        isCrouching = true;
+        capsuleCollider.height = crouchHeight;
+
+    }
+    public void StopCrouch()
+    {
+        if (Physics.Raycast(transform.position, Vector3.up, 1f))
+        {
+            return;
+        }
+
+        isCrouching = false;
+        if (capsuleCollider != null)
+        {
+            capsuleCollider.height = standingHeight;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (playerInputActions != null)
+        {
+            playerInputActions.Player.Sprint.performed -= OnSprintPerformed;
+            playerInputActions.Player.Sprint.canceled -= OnSprintCanceled;
+            playerInputActions.Player.Crouch.performed -= OnCrouchPerformed;
+            playerInputActions.Player.Crouch.canceled -= OnCrouchCanceled;
+            playerInputActions.Player.Disable();
+            playerInputActions.Dispose();
+            playerInputActions = null;
+        }
+    }
 
 }
